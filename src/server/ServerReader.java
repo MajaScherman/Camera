1682770eadaf98d3662c13b5c32f8343f1c950d3
maya039 <maja.scherman@gmail.com>
2014-11-14 13:23:41 +0100

    Adding the given code to the server monitor

--- src/server/ServerReader.java
+++ src/server/ServerReader.java
@@ -1,5 +1,8 @@
 package server;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ServerReader {
 
 }
--- src/server/ServerWriter.java
+++ src/server/ServerWriter.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class ServerWriter {
+
 
 }
