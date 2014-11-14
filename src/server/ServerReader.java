b4bddd0ec4207fac94f0013e9876c4c01bb90187
maya039 <maja.scherman@gmail.com>
2014-11-14 13:58:09 +0100

    A short start on the reader

--- src/server/ServerReader.java
+++ src/server/ServerReader.java
@@ -2,7 +2,26 @@ package server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ServerReader {
+public class ServerReader extends Thread {
 
+	private ServerSocket serverSocket;
+
+	public ServerReader(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+
+	}
+
+	public void run() {
+		try {
+			// The 'accept' method waits for a client to connect, then
+			// returns a socket connected to that client.
+			Socket clientSocket = serverSocket.accept();
+
+		} catch (Exception e) {
+
+		}
+	}
 }
