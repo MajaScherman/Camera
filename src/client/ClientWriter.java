704e81e5d07d35259e9fff4d20162ae440e657dd
Denhi Huynh <Denhi_@hotmail.com>
2014-11-15 15:46:52 +0100

    added constructor and run method

--- src/client/ClientWriter.java
+++ src/client/ClientWriter.java
@@ -1,5 +1,20 @@
 package client;
 
-public class ClientWriter {
+import java.io.IOException;
+import java.io.OutputStream;
 
+public class ClientWriter extends Thread {
+	private ClientMonitor monitor;
+	private OutputStream os;
+
+	public ClientWriter(ClientMonitor mon, OutputStream stream) {
+		monitor = mon;
+		os = stream;
+	}
+
+	public void run(){
+		while(!isInterrupted()){
+			
+		}
+	}
 }
