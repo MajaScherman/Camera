641d88bae216a9de46b9264750d2f286060d2f41
maya039 <maja.scherman@gmail.com>
2014-11-18 15:31:02 +0100

    first version that maybe works of the monitor

--- src/server/ServerMonitor.java
+++ src/server/ServerMonitor.java
@@ -9,20 +9,14 @@ package server;
  * Adapted for Axis cameras by Roger Henriksson 
  */
 
-import java.net.*; // Provides ServerSocket, Socket
-import java.io.*; // Provides InputStream, OutputStream
+import java.io.IOException;
+import java.io.InputStream; // Provides InputStream, OutputStream
+import java.io.OutputStream;
+import java.net.Socket; // Provides ServerSocket, Socket
 
-import se.lth.cs.eda040.fakecamera.*; // Provides AxisM3006V
+import se.lth.cs.eda040.fakecamera.AxisM3006V; // Provides AxisM3006V
 
-/**
- * Itsy bitsy teeny weeny web server. Always returns an image, regardless of the
- * requested file name.
- */
 public class ServerMonitor {
-	// private Socket clientsocket;
-	// private String request;
-	// private int myPort;
-
 	/**
 	 * Writing what I think is necessary here for our version//Maja
 	 */
@@ -32,8 +26,18 @@ public class ServerMonitor {
 	private OutputStream os; // the os from the client, is used as the is from
 								// the server
 	private String request;
-	private int myPort;
+	private int port;
 	private boolean movieMode;
+	private AxisM3006V camera;
+	private String header;
+
+	public ServerMonitor(int port) {
+		this.port = port;
+		camera = new AxisM3006V();
+		camera.init();
+		camera.setProxy("argus-1.student.lth.se", port);
+
+	}
 
 	public synchronized void setMovieMode(boolean movieMode) {
 		this.movieMode = movieMode;
@@ -44,18 +48,21 @@ public class ServerMonitor {
 		return movieMode;
 	}
 
-	public synchronized void setPort(int myPort) {
-		this.myPort = myPort;
+	public synchronized void setPort(int port) {
+		this.port = port;
 		notifyAll();
 	}
 
 	public synchronized int getPort() {
-		return myPort;
+		return port;
 	}
 
 	public synchronized void setClientSocket(Socket clientSocket) {
 		this.clientSocket = clientSocket;
 		synchStreamsAndRequest();
+		readRequest();
+		readHeader();
+
 		notifyAll();
 	}
 
@@ -64,13 +71,16 @@ public class ServerMonitor {
 
 			is = clientSocket.getInputStream();
 			os = clientSocket.getOutputStream();
+		} catch (IOException e) {
+			System.out.println("Caught exception " + e);
+		}
+	}
 
-			// Read the request
-			request = getLine(is);
-
+	private synchronized String readHeader() {
+		try {
 			// The request is followed by some additional header lines,
 			// followed by a blank line. Those header lines are ignored.
-			String header;
+
 			boolean cont = true;
 			do {
 				header = getLine(is);
@@ -79,9 +89,35 @@ public class ServerMonitor {
 
 			System.out.println("HTTP request '" + request + "' received.");
 			notifyAll();
+			return header;
+
 		} catch (IOException e) {
 			System.out.println("Caught exception " + e);
+			notifyAll();
+			return null; // om det blir fel ev kommer hit om det blir tomt,
+							// finns inte någon header
 		}
+	}
+
+	private synchronized String readRequest() {
+		// gives the request to the writer
+		// Read the request
+		try {
+			request = getLine(is);
+		} catch (IOException e) {
+			System.out.println("Caught exception " + e);
+			e.printStackTrace();
+		}
+		return request;
+
+	}
+
+	public synchronized String getRequest() {
+		return request;
+	}
+
+	public synchronized String getHeader() {
+		return header;
 	}
 
 	public synchronized Socket getClientSocket() {
@@ -97,20 +133,25 @@ public class ServerMonitor {
 	}
 
 	/**
-	 * the following methods are created by Amnup and Emnup
-	 * 
-	 * @return
+	 * Read a line from InputStream 's', terminated by CRLF. The CRLF is not
+	 * included in the returned string.
 	 */
-	public synchronized String getRequest() {
-		// gives the request to the writer
-		return request;
-	}
+	private synchronized String getLine(InputStream s) throws IOException {
+		boolean done = false;
+		String result = "";
 
-	public synchronized void setRequest(String newReq) {
-		// sets the request for the writer
-		request = newReq;
-		notifyAll();
-	}
+		while (!done) {
+			int ch = s.read(); // Read
+			if (ch <= 0 || ch == 10) {
+				// Something < 0 means end of data (closed socket)
+				// ASCII 10 (line feed) means end of line
+				done = true;
+			} else if (ch >= ' ') {
+				result += (char) ch;
+			}
+		}
 
+		return result;
+	}
 
 }
