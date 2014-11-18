bc9367c1971ec3f85e176fce418b3c09fab88ce1
maya039 <maja.scherman@gmail.com>
2014-11-18 15:17:15 +0100

    removing copied code, writing som new metods

--- src/server/ServerWriter.java
+++ src/server/ServerWriter.java
@@ -1,6 +1,30 @@
 package server;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import se.lth.cs.eda040.fakecamera.AxisM3006V;
+
 
 public class ServerWriter {
+	private ServerSocket serverSocket;
+	private ServerMonitor mon;
+	private Socket clientSocket;
+	private volatile InputStream is;
+	private volatile OutputStream os;
+	private AxisM3006V camera;   
+	private int port;
+
+	public ServerWriter(ServerSocket serverSocket, ServerMonitor mon, AxisM3006V camera) {
+		this.serverSocket = serverSocket;
+		this.mon = mon;
+		this.camera = camera;
+		
+	
+	}
+	
+	
 
 }
