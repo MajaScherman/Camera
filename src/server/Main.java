da282b395296614b15e559d754425d4d0299b31f
maya039 <maja.scherman@gmail.com>
2014-11-18 15:36:59 +0100

    main was complaining on meeee (sadface)

--- src/server/Main.java
+++ src/server/Main.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		ServerMonitor theServer = new ServerMonitor(Integer.parseInt(args[0]));
-		try {
-			theServer.handleRequests();
-		} catch (IOException e) {
+	//	ServerMonitor theServer = new ServerMonitor(Integer.parseInt(args[0]));
+	//	try {
+			//theServer.handleRequests();
+	//	} catch (IOException e) {
 			System.out.println("Error!");
 			//theServer.destroy();
-			System.exit(1);
-		}
+	//		System.exit(1);
+//		}
 	}
 
 }
--- src/server/ServerWriter.java
+++ src/server/ServerWriter.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import se.lth.cs.eda040.fakecamera.AxisM3006V;
 
 
-public class ServerWriter {
+public class ServerWriter extends Thread {
 	private ServerSocket serverSocket;
 	private ServerMonitor mon;
 	private Socket clientSocket;
@@ -20,11 +20,11 @@ public class ServerWriter {
 	public ServerWriter(ServerSocket serverSocket, ServerMonitor mon, AxisM3006V camera) {
 		this.serverSocket = serverSocket;
 		this.mon = mon;
-		this.camera = camera;
-		
-	
+		this.camera = camera;	
 	}
-	
+	public void run(){
+		
+	}
 	
 
 }
