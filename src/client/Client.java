1842178bc46fe4c8b3ed77d44de36b1500747396
maya039 <maja.scherman@gmail.com>
2014-12-01 12:34:46 +0100

    Worked with main metods

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,12 +2,18 @@ package client;
 
 public class Client {
 
-	
 	ClientWriter writer;
+
 	public static void main(String[] args) {
-		SocketAddress adr = new SocketAddress("localhost", 7897);
-		ClientMonitor mon = new ClientMonitor(1, null);
-		
-		
+		SocketAddress adr1 = new SocketAddress("localhost", 7897);
+		SocketAddress adr2 = new SocketAddress("localhost", 7898);
+		SocketAddress[] addresses = new SocketAddress[2];
+		addresses[0] = adr1;
+		addresses[1] = adr2;
+		ClientMonitor mon = new ClientMonitor(1, addresses);
+		ClientReader cReader0 = new ClientReader(mon, 0);
+		ClientReader cReader1 = new ClientReader(mon, 1);
+		ClientWriter cWriter = new ClientWriter(mon, null);
+
 	}
 }
--- src/client/ClientReader.java
+++ src/client/ClientReader.java
@@ -6,8 +6,11 @@ public class ClientReader extends Thread {
 
 	/**
 	 * The reader reads incoming data on the client side.
-	 * @param m The monitor for the client
-	 * @param server the number of the server that the reader handles
+	 * 
+	 * @param m
+	 *            The monitor for the client
+	 * @param server
+	 *            the number of the server that the reader handles
 	 */
 	public ClientReader(ClientMonitor m, int serverNbr) {
 		monitor = m;
@@ -16,12 +19,12 @@ public class ClientReader extends Thread {
 
 	public void Run() {
 		while (!isInterrupted()) {
-		try {
-			monitor.listenToServer(serverNumber);
-		} catch (Exception e) {
-			System.out.println("Listentoserver in monitor is mean :(");
-			e.printStackTrace();
+			try {
+				monitor.listenToServer(serverNumber);
+			} catch (Exception e) {
+				System.out.println("Listentoserver in monitor is mean :(");
+				e.printStackTrace();
+			}
 		}
-		}	
 	}
 }
