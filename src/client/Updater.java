1e1fe900dda633351bff03dfe575c9de6127e741
Denhi Huynh <Denhi_@hotmail.com>
2014-12-01 13:31:31 +0100

    Added method for pushing images to gui

--- src/client/Updater.java
+++ src/client/Updater.java
@@ -1,17 +1,24 @@
 package client;
 
+import gui.GUI;
+
 public class Updater extends Thread {
 	private ClientMonitor monitor;
-	
+	private GUI gui;
+
 	// check if there's a new image
-	public Updater(ClientMonitor mon) {
+	public Updater(ClientMonitor mon, GUI gui) {
 		monitor = mon;
+		this.gui = gui;
 	}
 
 	public void run() {
 		while (!isInterrupted()) {
-			monitor.checkUpdate(); //Check if new image has arrived
-			//Here we know that its time to update the GUI with new image. So we should just refresh image in GUI. 
+			monitor.checkUpdate(); // Check if new image has arrived
+			// Here we know that its time to update the GUI with new image.
+			// So we should just refresh image in GUI.
+			// get image
+			// push image to gui
 		}
 	}
 }
--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -11,14 +11,15 @@ import javax.swing.border.Border;
 import se.lth.cs.eda040.fakecamera.AxisM3006V;
 
 public class GUI extends JFrame {
-	private ImageViewer imageViewer1, imageViewer2;
+	private ImageViewer imageViewer0, imageViewer1;
 	private JFrame frame;
 	private InfoPanel infoPanel;
 	private ButtonPanel buttonPanel;
 
 	private byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
-	
-	public static void main(String[] args){
+	public static final int MAXIMUM_NUMBER_OF_CAMERAS = 2;
+
+	public static void main(String[] args) {
 		GUI gui = new GUI();
 	}
 
@@ -26,32 +27,45 @@ public class GUI extends JFrame {
 		super();
 		frame = new JFrame();
 		frame.setSize(1000, 600);
-		frame.getContentPane().setLayout(new GridLayout(2,2));
+		frame.getContentPane().setLayout(new GridLayout(2, 2));
 		Border blackline = BorderFactory.createLineBorder(Color.black);
-		
-		//Image panel for the first camera
+
+		// Image panel for the first camera
+		imageViewer0 = new ImageViewer();
+		imageViewer0.setBackground(Color.GRAY);
+		imageViewer0.setBorder(blackline);
+		frame.getContentPane().add(imageViewer0);
+
+		// Image panel for the second camera
 		imageViewer1 = new ImageViewer();
 		imageViewer1.setBackground(Color.GRAY);
 		imageViewer1.setBorder(blackline);
 		frame.getContentPane().add(imageViewer1);
-		
-		//Image panel for the second camera
-		imageViewer2 = new ImageViewer();
-		imageViewer2.setBackground(Color.GRAY);
-		imageViewer2.setBorder(blackline);
-		frame.getContentPane().add(imageViewer2);
-		
-		//The panel containing information about states
+
+		// The panel containing information about states
 		infoPanel = new InfoPanel();
 		frame.getContentPane().add(infoPanel);
-		
-		//Panel containing the buttons
+
+		// Panel containing the buttons
 		buttonPanel = new ButtonPanel();
 		frame.getContentPane().add(buttonPanel);
 		frame.setVisible(true);
 	}
 
-	
+	public void setImage(int cameraIndex, byte[] image) {
+		if (cameraIndex < 0 || cameraIndex > MAXIMUM_NUMBER_OF_CAMERAS) {
+			System.out.println("Camera index is out of range");
+			// TODO Throw exception
+		}
+		switch (cameraIndex) {
+		case 0:
+			imageViewer0.refresh(image);
+			break;
+		case 1:
+			imageViewer1.refresh(image);
+			break;
+		}
+
+	}
 
 }
-
