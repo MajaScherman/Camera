3824d9e4e77dec637ff3e18c6725b4374b7f2194
maya039 <maja.scherman@gmail.com>
2014-12-04 21:44:25 +0100

    started working on delay label and removed sysos

--- src/gui/ImageViewer.java
+++ src/gui/ImageViewer.java
@@ -16,12 +16,13 @@ import javax.swing.border.Border;
 
 public class ImageViewer extends JPanel {
 	private ImageIcon icon;
-	private JLabel imageIcon, label1;
+	private JLabel imageIcon, label1, label2;
 
 	public ImageViewer() {
 		super();
 		icon = new ImageIcon();
 		imageIcon = new JLabel(icon);
+		//label2 = new JLabel("Java Code Geeks", icon, JLabel.CENTER);
 		this.setSize(200, 200);
 		
 		Border blackline = BorderFactory.createLineBorder(Color.black);
@@ -32,8 +33,9 @@ public class ImageViewer extends JPanel {
 		label1.setHorizontalAlignment(SwingConstants.CENTER);
 		setLayout(new BorderLayout());
 		// TODO set visible to label1!!!!!!
-		add(imageIcon, BorderLayout.NORTH);
-		add(label1, BorderLayout.SOUTH);
+
+		add(imageIcon);
+		//add(label2);
 
 	}
 
@@ -41,6 +43,7 @@ public class ImageViewer extends JPanel {
 		Image theImage = getToolkit().createImage(data);
 		getToolkit().prepareImage(theImage, -1, -1, null);
 		icon.setImage(theImage);
+		imageIcon.setText("GE MIG TEXT");
 		icon.paintIcon(this, this.getGraphics(), 5, 5);
 	}
 
